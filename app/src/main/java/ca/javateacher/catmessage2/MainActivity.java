a4350d2dfb0110662990751ec39a5398546826b7
Alex Tetervak <tetervak@sheridancollege.ca>
2020-09-23 22:51:16 -0400

    updated the libraries

--- app/src/main/java/ca/javateacher/catmessage2/MainActivity.java
+++ app/src/main/java/ca/javateacher/catmessage2/MainActivity.java
@@ -8,7 +8,6 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 
